38d82505391c17d7ebebcac52c1c3fc93ca054e8
Basil Eagle <basileagle@gmail.com>
2025-11-07 00:00:26 +0000

    minor readme edit

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 # shutdown active server before continuing
-# (assumes that the executable is in PATH on windows)
+# (assumes that the executable is in PATH)
 getit exit
 # Client executable (command line application)
 go build -o dist/getit.exe ./src  
-# Server executable (built as GUI to avoid console window on startup)
+# Server executable (built as GUI on Windows to avoid console window on startup)
 go build -ldflags -H=windowsgui -o dist/getitserver.exe ./src
 # restart server
 getitserver --runtime=server
